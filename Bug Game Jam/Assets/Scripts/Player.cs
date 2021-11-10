523e07203c0b3ee3f80683af9f02b463bd825b17
phelixxthecat <59580790+ylhs220-2019@users.noreply.github.com>
2021-11-09 21:53:05 -0800

    Player Movement

--- Bug Game Jam/Assets/Scripts/Player.cs
+++ Bug Game Jam/Assets/Scripts/Player.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Rigidbody2D playerRb;
+    public float speed;
+    public Animator animator;
+    Vector2 movement;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
         
+    }
+
+    void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
