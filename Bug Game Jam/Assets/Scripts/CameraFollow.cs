d7af60651d7fd4d01049e4d8a7f80e393429d44a
phelixxthecat <59580790+ylhs220-2019@users.noreply.github.com>
2021-11-12 00:51:19 -0800

    Camera Follows Player
    Changed what objs of player is attached to what

---
+++ Bug Game Jam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+    public float smoothSpeed = .125f;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothPosition;
+
+        transform.LookAt(target);
+    }
+}
