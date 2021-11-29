590e5856730bdc302ba660033cca7395577a171f
phelixxthecat <59580790+ylhs220-2019@users.noreply.github.com>
2021-11-29 01:18:00 -0800

    Wave system being implemented and 1st lvl designed

---
+++ Bug Game Jam/Assets/Scripts/WaveStarter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveStarter : MonoBehaviour
+{
+    public event EventHandler OnPlayerEnterTrigger;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        if(player != null)
+        {
+            // Player inside trigger area!\
+            Debug.Log("Player inside area 1");
+            OnPlayerEnterTrigger?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
