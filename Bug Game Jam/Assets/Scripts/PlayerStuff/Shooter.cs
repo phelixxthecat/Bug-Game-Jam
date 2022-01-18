6ecd763c4110a6b2c571dc092bfc7625da296ed3
phelixxthecat <59580790+ylhs220-2019@users.noreply.github.com>
2022-01-18 03:18:05 -0800

    Updated shotgun reworking changing guns

--- Bug Game Jam/Assets/Scripts/PlayerStuff/Shooter.cs
+++ Bug Game Jam/Assets/Scripts/PlayerStuff/Shooter.cs
@@ -20,7 +20,7 @@ public class Shooter : MonoBehaviour
         
         if(Input.GetKeyDown(KeyCode.Space) && Time.time > NextShot)
         {
-            Swap();
+            
             Shoot();
         }
     }
@@ -36,16 +36,13 @@ public class Shooter : MonoBehaviour
         }
 
         else if(shotgun == true)
-        {
-            GameObject bullet = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
-            GameObject bullet2 = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
-            GameObject bullet3 = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
-            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
-            Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
-            rb.AddForce(new Vector3(firePoint.right.x - Random.Range(0, .5f), firePoint.right.y + Random.Range(0, .5f), firePoint.right.z) * bulletSpeed * .5f, ForceMode2D.Impulse);
-            rb2.AddForce(firePoint.right * bulletSpeed, ForceMode2D.Impulse);
-            rb3.AddForce(new Vector3(firePoint.right.x + Random.Range(0, .5f), firePoint.right.y - Random.Range(0, .5f), firePoint.right.z) * bulletSpeed * .5f, ForceMode2D.Impulse);
+        { 
+            for(int i = 0; i < 4; i++)
+            {
+                GameObject bullet = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(new Vector3(firePoint.right.x + Random.Range(-.5f, .5f), firePoint.right.y + Random.Range(-.5f, .5f), firePoint.right.z) * bulletSpeed * .5f, ForceMode2D.Impulse);
+            }
             NextShot = Time.time + .5; 
         }
 
@@ -60,70 +57,15 @@ public class Shooter : MonoBehaviour
 
     private void Change()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKey(keyCode))
         {
-            if(basic == true)
+            switch(KeyCode)
             {
-                False();
-                shotgun = true;
-            }
 
-            else if(shotgun == true)
-            {
-                False();
-                sniper = true;
-            }
-
-            else if(sniper == true)
-            {
-                False();
-                basic = true;
             }
         }
-        else if(Input.GetKeyDown(KeyCode.E))
-        {
-            if(basic == true)
-            {
-                False();
-                sniper = true;
-            }
-
-            else if(sniper == true)
-            {
-                False();
-                shotgun = true;
-            }
-
-            else if(shotgun == true)
-            {
-                False();
-                basic = true;
-            }
-        }
+        
     }
 
-    private void Swap()
-    {
-        if(basic == true)
-        {
-            index = 0;
-        }
 
-        else if(shotgun == true)
-        {
-            index = 1;  
-        }
-
-        else if(sniper == true)
-        {
-            index = 2;
-        }
-    }
-
-    private void False()
-    {
-        basic = false;
-        shotgun = false;
-        sniper = false;
-    }
 }
