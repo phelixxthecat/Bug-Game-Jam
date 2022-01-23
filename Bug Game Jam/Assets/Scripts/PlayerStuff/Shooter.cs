face3e6d4ab3661c876efa533eda5e02be297c10
phelixxthecat <59580790+ylhs220-2019@users.noreply.github.com>
2022-01-22 22:45:50 -0800

    Added bouncy to bullet list, reworked changing guns system

--- Bug Game Jam/Assets/Scripts/PlayerStuff/Shooter.cs
+++ Bug Game Jam/Assets/Scripts/PlayerStuff/Shooter.cs
@@ -6,12 +6,16 @@ public class Shooter : MonoBehaviour
 {
     public Transform firePoint;
     public float bulletSpeed = 20f;
+    private int bulletCount;
     private int index;
     private bool basic = true;
-    private bool shotgun = false;
+    private bool shotgun = true;
     private bool sniper = false;
+    private bool fullAuto = false;
+    private bool bouncy = false;
     private double NextShot = 0;
     public GameObject[] bulletType = new GameObject[5];
+    public GameObject currentBullet = null;
 
     // Update is called once per frame
     private void Update()
@@ -19,23 +23,19 @@ public class Shooter : MonoBehaviour
         Change();
         
         if(Input.GetKeyDown(KeyCode.Space) && Time.time > NextShot)
-        {
-            
+        {  
             Shoot();
         }
     }
 
     private void Shoot()
     {
-        if(basic == true)
+        if(basic && currentBullet == bulletType[0]) 
         {
-            GameObject bullet = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
-            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            rb.AddForce(firePoint.right * bulletSpeed, ForceMode2D.Impulse);
-            NextShot = Time.time + .25; 
+            BulletSpawning(.25f, bulletSpeed);
         }
 
-        else if(shotgun == true)
+        else if(shotgun && currentBullet == bulletType[1])
         { 
             for(int i = 0; i < 4; i++)
             {
@@ -46,25 +46,55 @@ public class Shooter : MonoBehaviour
             NextShot = Time.time + .5; 
         }
 
-        else if(sniper == true)
+        else if(sniper && currentBullet == bulletType[2])
         {
-            GameObject bullet = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
-            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            rb.AddForce(firePoint.right * bulletSpeed * 2, ForceMode2D.Impulse);
-            NextShot = Time.time + 1; 
+            BulletSpawning(1.5f, bulletSpeed * 2);
+        }
+        
+        else if(fullAuto && currentBullet == bulletType[3])
+        {
+            while(Input.GetKeyDown(KeyCode.Space) && bulletCount > 0)
+            {
+                BulletSpawning(.1f, bulletSpeed);
+            }
+            
+        }
+        else if(bouncy && currentBullet == bulletType[4])
+        {
+            BulletSpawning(.1f, bulletSpeed); 
         }
     }
 
     private void Change()
     {
-        if(Input.GetKey(keyCode))
-        {
-            switch(KeyCode)
-            {
-
-            }
+        if(Input.GetKeyDown(KeyCode.Alpha1) && basic){
+            Equip(0);
         }
-        
+        else if(Input.GetKeyDown(KeyCode.Alpha2) && shotgun){
+            Equip(1);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3) && sniper){
+            Equip(2);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3) && fullAuto){
+            Equip(3);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3) && bouncy){
+            Equip(4);
+        }
+    }
+
+    private void BulletSpawning(float bulletTime, float bulletSpeed)
+    {
+        GameObject bullet = Instantiate(bulletType[index], firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * bulletSpeed, ForceMode2D.Impulse);
+        NextShot = Time.time + bulletTime; 
+    }
+
+    private void Equip(int bullet)
+    {
+        currentBullet = bulletType[bullet];
     }
 
 
