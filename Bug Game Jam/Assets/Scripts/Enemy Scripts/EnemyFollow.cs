e58d95997b03594bee2f6b17c390fc130d767e40
phelixxthecat <67930741+phelixxthecat@users.noreply.github.com>
2023-07-14 01:48:18 -0700

    Enemies adjusted (kinda) and walls added for each level
    Need to
    - Add enemies to rooms
    - Fix Room spawning layouts
    - GUI
    - Play test!

--- Bug Game Jam/Assets/Scripts/Enemy Scripts/EnemyFollow.cs
+++ Bug Game Jam/Assets/Scripts/Enemy Scripts/EnemyFollow.cs
@@ -7,6 +7,11 @@ public class EnemyFollow : MonoBehaviour
     public GameObject player;
     private Rigidbody2D enemyRb;
     public float speed;
+    public float targetTime = 2;
+    public bool ableToDash = false;
+    public float dashSpeed;
+    public bool dash;
+    private float dashTime = 1;
     private Vector3 directionToPlayer;
 
     // Start is called before the first frame update
@@ -14,12 +19,36 @@ public class EnemyFollow : MonoBehaviour
     {
         enemyRb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");
+        dashSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(ableToDash)
+        {
+            DashCheck();
+        }
         directionToPlayer = (player.transform.position - transform.position).normalized;
-        enemyRb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * speed;
+        enemyRb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * dashSpeed;
+    }
+
+    private void DashCheck()
+    {
+        if (targetTime > 0)
+        {
+            targetTime -= Time.deltaTime;
+            dashSpeed = speed;
+        }
+        else if(targetTime <= 0 && dashTime > 0)
+        {
+            dashTime -= Time.deltaTime;
+            dashSpeed = speed * 2;
+        }
+        else
+        {
+            targetTime = 2;
+        }
+        
     }
 }
